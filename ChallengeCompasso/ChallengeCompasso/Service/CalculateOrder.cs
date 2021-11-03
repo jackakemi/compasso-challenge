6b3175ba77e495f8f76737507474f129fcb43ffc
jackeline.moura <jackeline.moura@indigosoft.com.br>
2021-11-03 00:21:21 -0300

    update Startup class

--- ChallengeCompasso/ChallengeCompasso/Service/CalculateOrder.cs
+++ ChallengeCompasso/ChallengeCompasso/Service/CalculateOrder.cs
@@ -1,8 +1,5 @@
 ﻿using ChallengeCompasso.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ChallengeCompasso.Service
 {
--- ChallengeCompasso/ChallengeCompasso/Startup.cs
+++ ChallengeCompasso/ChallengeCompasso/Startup.cs
@@ -39,10 +39,11 @@ namespace ChallengeCompasso
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChallengeCompasso v1"));
+                
             }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChallengeCompasso v1"));
 
             app.UseHttpsRedirection();
 
